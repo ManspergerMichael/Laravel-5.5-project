c09149e78f830b9c3dba47b0f344e58e917067e9
Michael Mansperger <ManspergerMichael@gmail.com>
2019-07-30 11:09:00 -0700

    Set up database connection
    Passed data to a view from the db

---
+++ Databases_and_Migrations/app/Http/Controllers/ProjectsController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Project;
+
+use Illuminate\Http\Request;
+
+class ProjectsController extends Controller
+{
+    //
+    public function index(){
+        //one way to query
+        //$projects = \App\Project::all();
+        $projects = Project::all();
+
+        //this returns the query results as json objects
+        //return $projects;
+        return view('projects.index',['projects' => $projects]);
+    }
+}
