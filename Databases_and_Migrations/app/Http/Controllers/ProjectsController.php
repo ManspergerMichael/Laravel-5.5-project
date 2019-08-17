0b586fcbbcd012895678cc38375a56d2c149f7b2
Michael Mansperger <ManspergerMichael@gmail.com>
2019-08-17 14:09:57 -0700

    fixed update function

--- Databases_and_Migrations/app/Http/Controllers/ProjectsController.php
+++ Databases_and_Migrations/app/Http/Controllers/ProjectsController.php
@@ -39,9 +39,13 @@ class ProjectsController extends Controller
         $project = Project::find($id);
         return view('projects.edit', compact('project'));
     }
-    public function update()
+    public function update($id)
     {
-
+        $project = Project::find($id);
+        $project->title = request('title');
+        $project->description = request('description');
+        $project->save();
+        return redirect('/projects');
     }
     public function destroy()
     {
--- Databases_and_Migrations/resources/views/projects/edit.blade.php
+++ Databases_and_Migrations/resources/views/projects/edit.blade.php
@@ -6,17 +6,19 @@
 @section('content')
     <h1>Edit Project</h1>
 
-    <form action="" method="post">
+    <form action="/projects/{{$project->id}}" method="POST">
+        {{ method_field('PATCH') }}
+        {{csrf_field()}}
         <div class="form-group">
             <label class="label" for="title">Title</label>
-            <input type="text" class="form-control" name=title placeholder="Title">
+            <input type="text" class="form-control" name=title placeholder="Title" value="{{$project->title}}">
         </div>
         <div class="form-group">
-            <label for="title">Title</label>
-            <textarea type="text" class="form-control" name=title placeholder="Discription"></textarea>
+            <label for="description">Description</label>
+            <textarea type="text" class="form-control" name="description" placeholder="Discription">{{$project->description}}</textarea>
         </div>
         <div class="form-group">
-            <button class="btn btn-success" type="submit">Edit Project</button>
+            <button class="btn btn-success" type="submit">Update Project</button>
         </div>
 
 
