d64855b319ba7477f90046875b05de7f48a6d3c0
Michael Mansperger <ManspergerMichael@gmail.com>
2019-08-19 00:48:40 -0700

    petstore crud

--- Pet_Store/app/Http/Controllers/PetStore.php
+++ Pet_Store/app/Http/Controllers/PetStore.php
@@ -8,17 +8,38 @@ use App\pets;
 class PetStore extends Controller
 {
     //
-    public function LogAndReg(){
-        return view ('Pets.LogAndReg');
-    }
+    
     public function create(){
         return view ('Pets.create');
     }
-    public function edit(){
+    public function edit($id){
         return view ('Pets.edit');
+    }
+    public function show($id){
+        return view ('Pets.show');
+    }
+    public function update($id){
+        return redirect('Pets.index');
+    }
+    public function destroy($id){
+        return redirect ('Pets.index');
     }
     public function index(){
         $pets = pets::all();
         return view ('Pets.index', ['pets' => $pets]);
     }
 }
+
+Route::get('/projects', 'ProjectsController@index');
+
+Route::get('/projects/create', 'ProjectsController@create');
+
+Route::get('/projects/{project}', 'ProjectsController@show');
+
+Route::post('/projects', 'ProjectsController@store');
+
+Route::get('/projects/{project}/edit', 'ProjectsController@edit');
+
+Route::patch('/projects/{project}', 'ProjectsController@update');
+
+Route::delete('/projects/{project}', 'ProjectsController@destroy');
