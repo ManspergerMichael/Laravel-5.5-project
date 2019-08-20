4573d4664ec6059f6befaff2022c01d7d5638319
Michael Mansperger <ManspergerMichael@gmail.com>
2019-08-20 14:21:51 -0700

    stuck
    @method('patch') and @csrf are intrpreted as text for some reason

--- Pet_Store/resources/views/Pets/edit.blade.php
+++ Pet_Store/resources/views/Pets/edit.blade.php
@@ -5,39 +5,41 @@
 
 <h1>Edit a pet</h1>
 
-<div>
 <form action="/Pets/{{$pet->id}}" method="POST">
-        @method('PATCH')
-        @csrf
 
-        <div class="form-group">
-            <label class="label" for="Pet_Name">Name</label>
-            <input type="text" class="form-control" name=Pet_Name placeholder="Pet Name">
+    @method('PATCH')
+    @csrf
+    
+    <div class="form-group">
+        <label class="label" for="Pet_Name">Name</label>
+        <input type="text" class="form-control" name=Pet_Name value="{{$pet->Pet_Name}}">
+    </div>
+    <div class="form-group">
+            <label class="label" for="type">Type</label>
+            <input type="text" class="form-control" name=type value="{{$pet->Type}}">
         </div>
-        <div class="form-group">
-                <label class="label" for="type">Type</label>
-                <input type="text" class="form-control" name=type placeholder="type">
-            </div>
-        <div class="form-group">
-            <label class="label" for="age">Age</label>
-            <input type="number" class="form-control" name=age>
-        </div>
-        <div class="form-group">
-            <label class="label" for="price">Price</label>
-            <input type="number" class="form-control" name=price>
-        </div>
-        <div >
-            <button class="btn btn-success" type="submit">Edit Pet</button>
-        </div>
-    </form>
-</div>
+    <div class="form-group">
+        <label class="label" for="age">Age</label>
+        <input type="number" class="form-control" name=age value="{{$pet->Age}}">
+    </div>
+    <div class="form-group">
+        <label class="label" for="price">Price</label>
+        <input type="number" class="form-control" name=price value="{{$pet->Price}}">
+    </div>
+    <div >
+        <button class="btn btn-success" type="submit">Edit Pet</button>
+    </div>
+</form>
 
-<div>
-    <form action="/Pets/{{$pet->id}}" method="POST">
-        @method('DELETE')
-        @csrf
-        <button class="btn btn-danger" type="submit">Edit Pet</button>
-    </form>
-</div>
+
+
+<form action="/Pets/{{$pet->id}}" method="POST">
+
+    @method('DELETE')
+    @csrf
+    
+    <button class="btn btn-danger" type="submit">Pet Adopted</button>
+</form>
+
 
 @endsection
