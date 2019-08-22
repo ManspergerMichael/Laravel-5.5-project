baee043300e573d56ea0d7c205f13f7328b47314
Michael Mansperger <ManspergerMichael@gmail.com>
2019-08-22 00:10:41 -0700

    Done playing with bootstrap

--- Databases_and_Migrations/resources/views/projects/create.blade.php
+++ Databases_and_Migrations/resources/views/projects/create.blade.php
@@ -9,15 +9,33 @@
 
         @csrf
 
-        <div>
-            <input type="text" name="title" placeholder="Project Name">
+        <div class="form-group">
+            <label class="label" for="title">Title</label>
+            <input class="form-control{{ $errors->has('title') ? 'has-error' : '' }}" type="text" name="title">
         </div>
-        <div>
-            <textarea name="description" placeholder="Project Description"></textarea>
+        <div class="form-group" >
+            <label class="label" for="description">Description</label>
+            <textarea class="form-control" name="description" ></textarea>
         </div>
         <div>
             <button type="submit">Create Project</button>
         </div>
     </form>
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{$error}}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
 
 @endsection
+
+{{--
+    Dynamic code that would in thery add the has-error class to the input and textarea if errors are present.
+    Dosnet work for some reason. Errors div works fine. I might revisit this if I want to include a red
+    border when errors are found. Unlikely. Will keep this here as a note. 8-22-19
+    {{ $errors->has('description') ? "has-error" : '' }}
+--}}
