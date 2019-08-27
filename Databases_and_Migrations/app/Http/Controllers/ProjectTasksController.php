0a261d1776dd2fd6252dfa0828f9d29a17b462b2
Michael Mansperger <ManspergerMichael@gmail.com>
2019-08-27 14:58:37 -0700

    encapsulation

--- Databases_and_Migrations/app/Http/Controllers/ProjectTasksController.php
+++ Databases_and_Migrations/app/Http/Controllers/ProjectTasksController.php
@@ -8,22 +8,42 @@ use App\Project;
 
 class ProjectTasksController extends Controller
 {
+    /*
+        A note on encapsulation
+        encapsulation is containing the model logic in the model
+        the controller should not contain code for how the data and model interact
+        the controller should only call functions and pass data to the model
+     */
 
     public function store(Project $project)
     {
-        /* $attributes = request()->validate(['description' => 'required|max:255']);
-        $project->addTask($attributes['description']); */
+        $attributes = request()->validate(['description' => 'required|max:255']);
+        $project->addTask($attributes['description']);
+
+        /*
+        Funcion as shown in lessons dosent work
         $project->addTask(
             request()->validate(['description' => 'required'])
         );
+        */
         return back();
     }
 
     public function update(Task $task)
     {
-        $task->update([
-            'completed' => request()->has('completed')
-        ]);
+        /*
+        if(request()->has('completed'))
+        {
+            $task->complete();
+        }
+        else
+        {
+            $task->incomplete();
+        }
+        */
+
+        request()->has('completed') ? $task->complete() : $task->incomplete();
+
         return back();
          // dd($task);
     }
