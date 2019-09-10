eedf2dfe0b8202f883f7ffa7c4ef5fb6de2eeaa5
Michael Mansperger <ManspergerMichael@gmail.com>
2019-09-10 16:26:40 -0700

    Restarting practice project

--- Pet_Store/routes/web.php
+++ Pet_Store/routes/web.php
@@ -11,29 +11,6 @@
 |
 */
 
-
-/* Route::get('/', "PetStore@LogAndReg");
-Route::get('/index', "PetStore@index");
-Route::get('/edit', "PetStore@edit");
-Route::get('/create',"PetStore@create"); */
-Route::resource('Pets', 'PetStore');
-
-/*
---Format of restful routing created with the resource helper
-
-
-Route::get('/Pets', 'PetStore@index');
-
-Route::get('/projects/create', 'ProjectsController@create');
-
-Route::get('/projects/{project}', 'ProjectsController@show');
-
-Route::post('/projects', 'ProjectsController@store');
-
-Route::get('/projects/{project}/edit', 'ProjectsController@edit');
-
-Route::patch('/projects/{project}', 'ProjectsController@update');
-
-Route::delete('/projects/{project}', 'ProjectsController@destroy');
-*/
-
+Route::get('/', function () {
+    return view('welcome');
+});
